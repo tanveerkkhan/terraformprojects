3ef749b90dd62c1c6f04a73d6f4e000c27b4745f
tanveerkkhan <tanveerkkhan89@gmail.com>
2026-09-14 12:55:34 +0530

    added backend

--- mdec2/main.tf
+++ mdec2/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "tanveer-terraform-state-bucket"
+    key            = "ec2-module/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 module "instance" {
   source        = ".//mod"
   instance_type = "t3.micro"
